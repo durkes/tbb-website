cf6b49562921aa57e3a22b0e39cf88fd79fbbec1
durkes <durkes@users.noreply.github.com>
2023-05-05 15:07:43 -0500

    Add contact input selector specificity

--- source/js/_incl/contact.js
+++ source/js/_incl/contact.js
@@ -8,13 +8,13 @@ jQuery(document).ready(function ($) {
         $('#contact_form input[required], #contact_form textarea[required]').each(function () {
             $(this).css('background-color', '');
             if (!$.trim($(this).val())) { //if this field is empty 
-                $(this).css('background-color', '#FFDEDE'); //change border color to #FFDEDE   
+                $(this).css('background-color', '#FFDEDE'); //change border color to #FFDEDE 
                 proceed = false; //set do not proceed flag
             }
             //check invalid email
             var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
             if ($(this).attr('type') === 'email' && !email_reg.test($.trim($(this).val()))) {
-                $(this).css('background-color', '#FFDEDE'); //change border color to #FFDEDE   
+                $(this).css('background-color', '#FFDEDE'); //change border color to #FFDEDE 
                 proceed = false; //set do not proceed flag				
             }
         });
@@ -23,16 +23,16 @@ jQuery(document).ready(function ($) {
         {
             //get input field values data to be sent to server
             var post_data = {
-                'user_name': $('input[name=name]').val(),
-                'user_email': $('input[name=email]').val(),
-                'subject': $('input[name=subject]').val(),
-                'msg': $('textarea[name=message]').val()
+                'user_name': $('#contact_form input[name=name]').val(),
+                'user_email': $('#contact_form input[name=email]').val(),
+                'subject': $('#contact_form input[name=subject]').val(),
+                'msg': $('#contact_form textarea[name=message]').val()
             };
 
             //Ajax post data to server
             $.post('php/sendmail.php', post_data, function (response) {
                 var output;
-                if (response.type === 'error') { //load json data from server and output message     
+                if (response.type === 'error') { //load json data from server and output message 
                     output = '<br><br><div class="error">' + response.text + '</div>';
                 } else {
                     output = '<br><br><div class="success">' + response.text + '</div>';
@@ -48,7 +48,7 @@ jQuery(document).ready(function ($) {
     });
 
     //reset previously set border colors and hide all message on .keyup()
-    $('#contact_form  input[required=true], #contact_form textarea[required=true]').keyup(function () {
+    $('#contact_form input[required=true], #contact_form textarea[required=true]').keyup(function () {
         $(this).css('background-color', '');
         $('#result').slideUp();
     });
